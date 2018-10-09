9572c54ae8ce5042ef7040dab5f3ad5eafc09726
Williano <paawilly17@gmail.com>
2018-10-09 17:39:54 +0000

    [ADD] :white_check_mark: Add first xcode project

---
+++ GettingStarted/GettingStarted/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GettingStarted
+//
+//  Created by William Kpabitey Kwabla on 10/9/18.
+//  Copyright © 2018 William Kpabitey Kwabla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
