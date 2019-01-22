6b29b657238c32ec3ec20d087bc6b2baa0a760de
Williano <paawilly17@gmail.com>
2019-01-22 18:58:04 +0000

    [NEW CODE] :white_check_mark: Add code  for learning navigation

---
+++ TrafficSegues/TrafficSegues/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TrafficSegues
+//
+//  Created by William Kpabitey Kwabla on 1/22/19.
+//  Copyright © 2019 William Kpabitey Kwabla. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+        
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = textField.text
+    }
+}
+
